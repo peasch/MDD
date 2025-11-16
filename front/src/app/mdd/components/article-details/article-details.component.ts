6f73500b362c3774046f4cc9279e29bc1dfab701
Pierre-Alain Schaming <peaschaming@gmail.com>
2025-11-16 18:11:29 +0100

    removing remaining errors in browser

--- front/src/app/mdd/components/article-details/article-details.component.ts
+++ front/src/app/mdd/components/article-details/article-details.component.ts
@@ -1,27 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Article } from "../../models/article.model";
 import { Comment } from "../../models/comment.model";
 import { ArticleService } from "../../../shared/services/article.service";
 import { ActivatedRoute, Router } from "@angular/router";
 import { CommentService } from "../../../shared/services/comment.service";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { User } from "../../models/user.model";
-import { UserService } from "../../../shared/services/user.service";
-import {ThemeService} from "../../../shared/services/theme.service";
-import {Theme} from "../../models/theme.model";
+import { ThemeService } from "../../../shared/services/theme.service";
+import { Theme } from "../../models/theme.model";
 
-/**
- * Page d’affichage des détails d’un article.
- *
- * Ce composant :
- * - charge l’article sélectionné ;
- * - récupère et trie les commentaires associés ;
- * - permet d’ajouter un commentaire via un formulaire réactif ;
- * - redirige vers la page 404 en cas d’erreur de chargement.
- *
- * @example
- * <app-article-details></app-article-details>
- */
 @Component({
   selector: 'app-article-details',
   templateUrl: './article-details.component.html',
@@ -30,30 +16,17 @@ import {Theme} from "../../models/theme.model";
 export class ArticleDetailsComponent implements OnInit {
 
   /** Article à afficher (chargé dynamiquement). */
-  article!: Article;
-  theme!:Theme;
+  article: Article | null = null;
+  theme: Theme | null = null;
 
   /** Ensemble des commentaires liés à l’article. */
-  comments!: Comment[];
+  comments: Comment[] = [];
 
-  /**
-   * Formulaire réactif utilisé pour publier un commentaire.
-   *
-   * Champs :
-   * - content : contenu du commentaire (min. 3 caractères, obligatoire)
-   */
+  /** Formulaire d’ajout de commentaire */
   public form = this.fb.group({
-    content: ['', [Validators.required, Validators.min(3)]]
+    content: ['', [Validators.required, Validators.minLength(3)]]
   });
 
-  /**
-   * @param articleService Service permettant de récupérer les détails d’un article.
-   * @param commentService Service gérant les commentaires d’un article.
-   * @param userService Service permettant de récupérer les informations des utilisateurs.
-   * @param fb FormBuilder pour construire les formulaires réactifs.
-   * @param route Route active permettant de récupérer le paramètre `id`.
-   * @param router Service Angular de navigation.
-   */
   constructor(
     private articleService: ArticleService,
     private commentService: CommentService,
@@ -63,12 +36,6 @@ export class ArticleDetailsComponent implements OnInit {
     private router: Router
   ) {}
 
-  /**
-   * Initialise le composant :
-   * - récupère l’ID depuis l’URL ;
-   * - charge l’article correspondant ;
-   * - charge et trie les commentaires (du plus récent au plus ancien).
-   */
   ngOnInit(): void {
     const id = this.route.snapshot.params['id'];
 
@@ -76,13 +43,16 @@ export class ArticleDetailsComponent implements OnInit {
     this.articleService.getArticleById(id).subscribe({
       next: (response: any) => {
         this.article = response.article;
-        this.themeService.getThemeById(response.article.themeId).subscribe( {
-          next:(response: any) => {
-            console.log(response);
-            this.theme = response.theme;
+
+        // Chargement du thème associé
+        this.themeService.getThemeById(response.article.themeId).subscribe({
+          next: (themeResponse: any) => {
+            this.theme = themeResponse.theme;
+          },
+          error: (error) => {
+            console.error('Erreur lors du chargement du thème :', error);
           }
-          }
-        )
+        });
       },
       error: (error) => {
         console.error('Error loading article:', error);
@@ -93,7 +63,6 @@ export class ArticleDetailsComponent implements OnInit {
     // Chargement des commentaires
     this.commentService.getCommentsOfArticle(id).subscribe({
       next: (response: any) => {
-
         if (Array.isArray(response.comments)) {
           this.comments = response.comments.sort(
             (a: any, b: any) =>
@@ -103,8 +72,6 @@ export class ArticleDetailsComponent implements OnInit {
           this.comments = [];
           console.warn('La réponse ne contient pas de commentaires valides.');
         }
-
-        console.log('Commentaires triés :', this.comments);
       },
       error: (error) => {
         console.error('Erreur lors du chargement des commentaires :', error);
@@ -115,18 +82,17 @@ export class ArticleDetailsComponent implements OnInit {
 
   /**
    * Ajoute un commentaire à l’article.
-   *
-   * @param id Identifiant de l’article concerné.
-   * @param form Formulaire contenant le contenu du commentaire.
    */
   addComment(id: number, form: FormGroup): void {
+    if (form.invalid) return;
+
     const content = form.get('content')?.value;
 
     this.commentService.addComment(id, content).subscribe({
       next: () => {
-        // Recharge la page d’article sans recharger l’ensemble du site
+        // Recharge la page d’article sans recharger le site
         this.router.navigateByUrl('/mdd/articles', { skipLocationChange: true })
-          .then(() => this.router.navigate(['/mdd/articles/' + id]));
+          .then(() => this.router.navigate(['/mdd/articles', id]));
       },
       error: (error) => {
         console.error('Error adding comment:', error);
--- front/src/app/mdd/components/comment/comment.component.ts
+++ front/src/app/mdd/components/comment/comment.component.ts
@@ -4,15 +4,6 @@ import { User } from "../../models/user.model";
 import { UserService } from "../../../shared/services/user.service";
 import { Router } from "@angular/router";
 
-/**
- * Composant d’affichage d’un commentaire.
- *
- * Il reçoit un commentaire en entrée et récupère automatiquement
- * les informations de l’auteur associées à ce commentaire.
- *
- * @example
- * <app-comment [comment]="comment"></app-comment>
- */
 @Component({
   selector: 'app-comment',
   templateUrl: './comment.component.html',
@@ -20,34 +11,25 @@ import { Router } from "@angular/router";
 })
 export class CommentComponent implements OnInit {
 
-  /**
-   * Commentaire à afficher.
-   *
-   * Le composant parent doit fournir un objet `Comment`.
-   */
   @Input() comment!: Comment;
 
   /** Informations de l’auteur du commentaire. */
-  author!: User;
+  author: User | null = null;
 
-  /**
-   * @param userService Service permettant de récupérer les informations d’un utilisateur.
-   * @param router Service de navigation (utilisé en cas d’erreur pour redirection).
-   */
   constructor(
     private userService: UserService,
     private router: Router
   ) {}
 
-  /**
-   * Initialise le composant :
-   * - récupère l’auteur du commentaire via son `authorId`.
-   * - redirige vers la page 404 en cas d’erreur.
-   */
   ngOnInit(): void {
+    // Sécurité : si jamais le parent ne fournit pas correctement le comment
+    if (!this.comment || !this.comment.authorId) {
+      console.warn('Comment ou authorId manquant dans CommentComponent', this.comment);
+      return;
+    }
+
     this.userService.getUserById(this.comment.authorId).subscribe({
       next: (response: any) => {
-        console.log(response.user);
         this.author = response.user;
       },
       error: (error) => {
